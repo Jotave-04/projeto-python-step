30316cb3c51c78da7f9d5bb0924c4e5758ed500d
Jotave-04 <jvcanavez@gmail.com>
2026-04-11 16:40:47 -0300

    Adicionando arquivos da aula2 e limpando projeto antigo

---
+++ aula2/app/models/usuario_model.py
@@ -0,0 +1,9 @@
+from app.database.supabase_client import supabase
+
+def listar_usuarios():
+    response = supabase.table("usuarios").select("*").execute()
+    return response.data
+
+def criar_usuario(nome):
+    response = supabase.table("usuarios").insert({"nome": nome}).execute()
+    return response.data
