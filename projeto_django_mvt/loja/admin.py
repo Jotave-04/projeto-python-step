7ed0c6fe578f0448a51409b4c49a3e44b335ffc7
Jotave-04 <jvcanavez@gmail.com>
2026-06-27 14:37:34 -0300

    Aula 7 - CMS - Sistema de Gerenciamento de Produtos com Django

---
+++ projeto_django_mvt/loja/admin.py
@@ -0,0 +1,11 @@
+# loja/admin.py
+from django.contrib import admin
+from .models import Produto
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'nome', 'preco', 'estoque', 'ativo', 'criado_em'
+    )
+    list_filter = ('ativo',)
+    search_fields = ('nome',)
---
+++ projeto_django_mvt/usuarios/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Template endpoints
+    path('', views.listar_template, name='listar_template'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
+]
