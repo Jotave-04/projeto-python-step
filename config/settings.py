f6662c35d4fec3417aa6994d54822d1384e66f83
Jotave-04 <jvcanavez@gmail.com>
2026-08-08 14:44:28 -0300

    API completa do projeto final - gestão de eventos com django

---
+++ config/settings.py
@@ -0,0 +1,67 @@
+from dotenv import load_dotenv
+from urllib.parse import urlparse
+from pathlib import Path
+import os
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.getenv("JWT_SECRET", "minha-chave-secreta-super-segura")
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["*"]
+
+INSTALLED_APPS = [
+    "django.contrib.contenttypes",
+    "django.contrib.auth",
+    "rest_framework",
+    "corsheaders",
+    "core",
+]
+
+MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+]
+
+ROOT_URLCONF = "config.urls"
+
+# ──────────────────────────────────────────────
+# CONFIGURAÇÃO DO BANCO — SUPABASE (PostgreSQL)
+# ──────────────────────────────────────────────
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError(
+        "DATABASE_URL não encontrada. "
+        "Crie um banco no Supabase e coloque a URL no .env"
+    )
+
+url = urlparse(DATABASE_URL)
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": url.path[1:],       # remove a "/" do início do path
+        "USER": url.username,
+        "PASSWORD": url.password,
+        "HOST": url.hostname,
+        "PORT": url.port,
+    }
+}
+
+CORS_ALLOW_ALL_ORIGINS = True
+'''
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+'''
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+}
